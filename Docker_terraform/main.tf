271fa18dd69264b959606130563bae3dc20b899b
root <root@DESKTOP-Q74HK5H.localdomain>
2025-06-01 13:39:11 +0300

    create custom network configuration

--- Docker_terraform/main.tf
+++ Docker_terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-required_providers {
-docker = {
-source = "kreuzwerker/docker"
-version = "~> 3.0.1"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
   }
- }
 }
 provider "docker" {}
 // Create Image for a Container
@@ -14,12 +14,29 @@ resource "docker_image" "nginx" {
 }
 // Create a Container with above image
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name = "Web1"  
+  image    = docker_image.nginx.image_id
+  name     = "Web1"
   hostname = "web_server1"
-  dns = [ "192.168.1.1", "8.8.8.8" ]
+  dns      = ["192.168.1.1", "8.8.8.8"]
+  network_advanced {
+    name       = docker_network.New_Network.id
+    depends_on = [docker_network.New_Network]
+
+  }
   ports {
     internal = 80
     external = 8001
   }
+}
+//create a custom network in docker to use it with our container 
+resource "docker_network" "New_Network" {
+  name = "terraform_network"
+  ipam_config {
+    aux_address = {}
+    gateway     = "12.12.12.1"
+    ip_range    = ""
+    subnet      = "12.12.12.0/24"
+  }
+
+
 }
