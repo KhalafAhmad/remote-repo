52f974618b47cf22b35c9942c1220114e65e305b
root <root@DESKTOP-Q74HK5H.localdomain>
2025-06-01 11:57:21 +0300

    add custom hostname and dns entry

--- Docker_terraform/main.tf
+++ Docker_terraform/main.tf
@@ -16,6 +16,8 @@ resource "docker_image" "nginx" {
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
   name = "Web1"  
+  hostname = "web_server1"
+  dns = [ "192.168.1.1", "8.8.8.8" ]
   ports {
     internal = 80
     external = 8001
